cebe85d50df2fb4c9c058d8020edb50ab9636b05
libole <libole@bytedance.com>
2025-06-20 17:49:41 +0800

    translator.thrift

---
+++ hertz_gorm/idl/translator.thrift
@@ -0,0 +1,13 @@
+namespace go translator
+
+struct TranslateRequest {
+    1: string text
+}
+
+struct TranslateResponse {
+    1: string translation
+}
+
+service TranslatorService {
+    TranslateResponse TranslateText(1: TranslateRequest req)
+}
